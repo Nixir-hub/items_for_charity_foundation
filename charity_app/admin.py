fa4462f91a8faabc666e3ba97d56c437ec79702b
Nixir-hub <nixir@hotmail.com>
2021-11-22 10:12:05 +0100

    add models: Category, Institution, Donation

--- charity_app/admin.py
+++ charity_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from charity_app.models import Category, Donation, Institution
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Donation)
+admin.site.register(Institution)
--- charity_app/models.py
+++ charity_app/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    FOUNDATION = {
+        (1, "fundacja"),
+        (2, "organizacja pozarządowa"),
+        (3, "zbiórka lokalna"),
+    }
+    type = models.IntegerField(choices=FOUNDATION, default=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.TextField()
+    phone_number = models.PositiveIntegerField(max_length=9)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
