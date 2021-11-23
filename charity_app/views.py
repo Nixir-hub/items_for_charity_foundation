5d5d6aecc0073367e8f17f7b799f763a6b7a47b4
Nixir-hub <nixir@hotmail.com>
2021-11-23 13:33:52 +0100

    templates changes, login.html

--- charity_app/views.py
+++ charity_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 
-from charity_app.models import Institution
+from charity_app.models import Institution, Donation
 
 
 class LandingPage(View):
@@ -13,6 +13,10 @@ class LandingPage(View):
         foundList = Institution.objects.all().filter(type=1)
         nationalOffList = Institution.objects.all().filter(type=2)
         localList = Institution.objects.all().filter(type=3)
+        sumLst = len(foundList) + len(nationalOffList) + len(localList)
+        sum_packages = 0
+        for pack in Donation.objects.all():
+            sum_packages += int(pack.quantity)
         paginator1 = Paginator(foundList, 3)
         paginator2 = Paginator(nationalOffList, 3)
         paginator3 = Paginator(localList, 3)
@@ -22,7 +26,8 @@ class LandingPage(View):
         page_obj2 = paginator2.get_page(page_number2)
         page_number3 = request.GET.get("page")
         page_obj3 = paginator3.get_page(page_number3)
-        return render(request, "index.html", {"found": page_obj1, "offn": page_obj2, "local_found": page_obj3
+        return render(request, "index.html", {"found": page_obj1, "offn": page_obj2, "local_found": page_obj3,
+                                              "sum_lst": sumLst, "sum_pack": sum_packages
                                               })
 
 
